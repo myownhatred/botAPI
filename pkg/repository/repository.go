28deabb710766ca469e316d1e48031f81c89cfd7
Neerlyn <myownhatred@gmail.com>
2021-05-17 07:59:43 +0000

    repos+services+DI frame

---
+++ pkg/repository/repository.go
@@ -0,0 +1,24 @@
+package repository
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoItem
+	TodoList
+}
+
+func NewRepository() *Repository {
+	return &Repository{
+		Authorization: nil,
+		TodoItem:      nil,
+		TodoList:      nil,
+	}
+}
